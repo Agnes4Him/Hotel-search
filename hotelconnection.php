51794afb9bd9cd908900f66e11050e6d873f6ff2
Agnes4Him <ojuhagnes@gmail.com>
2021-09-27 10:40:20 +0100

    added extra files to secure database info

--- hotelconnection.php
+++ hotelconnection.php
@@ -1,6 +1,20 @@
 <?php 
 
-$link = mysqli_connect("localhost:8889", "root", "root", "hotel");
+require_once('vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+$dotenv->load();
+
+$host_name = $_ENV['HOST_NAME'];
+
+$user_name = $_ENV['USER_NAME'];
+
+$password = $_ENV['PASSWORD'];
+
+$database_name = $_ENV['DATABASE_NAME'];
+
+$link = mysqli_connect($host_name, $user_name, $password, $database_name);
 
 if(mysqli_connect_error()) {
 
