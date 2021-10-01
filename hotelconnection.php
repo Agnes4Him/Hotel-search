f856393911c71b5ff84740772cad762033180339
Agnes4Him <ojuhagnes@gmail.com>
2021-10-01 17:22:42 +0100

    Update

--- hotelconnection.php
+++ hotelconnection.php
@@ -2,20 +2,6 @@
 
 /*Connection to local database
 
-require_once('vendor/autoload.php');
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-
-$dotenv->load();
-
-$host_name = $_ENV['HOST_NAME'];
-
-$user_name = $_ENV['USER_NAME'];
-
-$password = $_ENV['PASSWORD'];
-
-$database_name = $_ENV['DATABASE_NAME'];
-
 $link = mysqli_connect($host_name, $user_name, $password, $database_name);*/
 
  //Connection to heroku database 
