1c0f6ad506e0d9d33ad0a6b794aec6d8cf20481c
Agnes4Him <ojuhagnes@gmail.com>
2021-10-05 16:21:17 +0100

    Edited hotelactions

--- hotelactions.php
+++ hotelactions.php
@@ -165,13 +165,13 @@ if($_GET['action'] == "newsletter") {
   }
 }
 
-require_once('vendor/autoload.php');
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-
-$dotenv->load();
-
 if($_GET['action'] == "getkeys") {
+
+  require_once('vendor/autoload.php');
+
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+  $dotenv->load();
 
   echo json_encode($_ENV);
 
