2a821164c7c793e03e94963a5fb54df2895e5247
Agnes4Him <ojuhagnes@gmail.com>
2021-10-06 11:09:00 +0100

    A commit message

--- hotelactions.php
+++ hotelactions.php
@@ -167,7 +167,7 @@ if($_GET['action'] == "newsletter") {
 
 if($_GET['action'] == "getkeys") {
 
-  require_once('/vendor/autoload.php');
+  require_once('vendor/autoload.php');
 
   $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 
